c8e3649f12573b5f41eee4c46affbdf93287f7af
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-25 15:54:24 +0200

    Condition accesso email

--- mail/main.js
+++ mail/main.js
@@ -1,4 +1,24 @@
 `use strict`
 
-const firstName = prompt ("Scrivi la tua email");
-console.log(firstName);
+/* Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
+
+/* lista di mail che possono accedere */
+const listMail = ["eleonora@gmail.com", "edoardo@gmail.com", "guenda@gmail.com", "macho@gmail.com"];
+listMail[0]
+listMail[1]
+listMail[2]
+listMail[3]
+console.log(listMail);
+
+const eMail = prompt ("Scrivi la tua email");
+console.log(eMail);
+
+const liberoAccesso = listMail[0];
+
+if (eMail === liberoAccesso) {
+    alert("l'utente ha l'accesso")
+} else {
+    alert("l'utente non è abilitato all'accesso")    
+}
